7e33938509d3b9569125569807c009a03f67d60a
Pooja Sree <poojasreepanapakala@gmail.com>
2026-06-01 09:37:44 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s.length() == t.length())
+        {
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i] != t[i])
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return true;
+    }
+};
