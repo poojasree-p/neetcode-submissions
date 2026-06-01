d189eae3ff2faac90780dc504407c10b5c0c6e1b
Pooja Sree <poojasreepanapakala@gmail.com>
2026-06-01 10:53:40 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<int, int> m1;
+        unordered_map<int, int> m2;
+        for(int i=0;i<s.length();i++)
+        {
+            m1[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            m2[t[i]]++;
+        }
+        if(m1 == m2)
+        {
+            return true;
+        }
+        return false;
+    }
+};
